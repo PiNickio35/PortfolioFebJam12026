de4c633a4bcc76c7a91c54bab4e4f9219e2c2e94
Draakslagter <simon@gssr.co.za>
2026-02-07 13:31:01 +0200

    Audio Fixed, WinLose Added

--- Portfolio Jam Feb 1 2026/Assets/Scripts/VolumeManager.cs
+++ Portfolio Jam Feb 1 2026/Assets/Scripts/VolumeManager.cs
@@ -1,41 +1,43 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using Slider = UnityEngine.UI.Slider;
 
 public enum AudioMixMode
 {
-    LinearAudioSourceVolume, LinearMixerVolume, LogarithmicMixerVolume
+    LinearMixerVolume, LogarithmicMixerVolume
 }
 public class VolumeManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private AudioSource audioSource;
     [SerializeField] private TextMeshProUGUI volumeText;
-    [SerializeField] private AudioMixMode _audioMixMode;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private AudioMixMode audioMixMode;
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey("Volume"))
-        {
-            audioMixer.SetFloat("Volume", Mathf.Log10(PlayerPrefs.GetFloat("Volume", 1)) * 20);
-        }
+        if (!PlayerPrefs.HasKey("Volume")) return;
+        volumeText.SetText($"Volume: {PlayerPrefs.GetFloat("Volume", 1)*100:F0}");
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+        audioMixer.SetFloat("Volume", Mathf.Log10(PlayerPrefs.GetFloat("Volume", 1)) * 20);
     }
 
     public void OnChangeSlider(float value)
     {
         volumeText.SetText($"Volume: {value * 100:F0}");
 
-        switch (_audioMixMode)
+        switch (audioMixMode)
         {
-            case AudioMixMode.LinearAudioSourceVolume:
-                audioSource.volume = value;
-                break;
             case AudioMixMode.LinearMixerVolume:
                 audioMixer.SetFloat("Volume", -80+ value *80);
                 break;
             case AudioMixMode.LogarithmicMixerVolume:
                 audioMixer.SetFloat("Volume", Mathf.Log10(value) * 20);
                 break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
         
         PlayerPrefs.SetFloat("Volume", value);
